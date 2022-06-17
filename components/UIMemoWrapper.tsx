7048979b4326f51ec8de6fb2fd96eba75ccaf8e8
Ben Flowers <ben.j.flowers@gmail.com>
2022-06-17 17:35:29 +0100

    initial

--- components/UIMemoWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React, { memo } from "react";
-
-// eslint-disable-next-line react/display-name
-export const UIMemoWrapper = memo((props: { children: React.ReactNode }) => (
-  <>{props.children}</>
-));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { UIHeader } from "../components/UIHeader";
-import { UITop } from "../components/UITop";
-import dynamic from "next/dynamic";
-import { UIValues } from "../components/UIValues";
-import { UIItems } from "../components/UIItems";
-import { UIFooter } from "../components/UIFooter";
-import useSWR from "swr";
-
-const Chart = dynamic(() => import("../components/UIChart"), {
-  loading: () => <p>Loading ...</p>,
-  ssr: false,
-});
-
-const portfolioQuery = `{
-  portfolio {
-    id
-    currentBalance {
-      goldBalance
-      platinumBalance
-    }
-    history {
-      date
-      totalValue {
-        currency
-        amount
-      }
-    }
-    portfolioItems {
-      image
-      sku
-      name
-      purchasePrice
-      weight
-      metal
-    }
-  }
-}`;
 
 const Home: NextPage = () => {
-  const portfolioResult = useSWR(portfolioQuery);
+
   return (
     <div>
       <Head>
@@ -48,19 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <UIHeader />
-
-      {portfolioResult.data && (
-        <main>
-          <UITop />
-          <Chart portfolio={portfolioResult.data.portfolio} />
-          <div className="p-6">
-            <UIValues portfolio={portfolioResult.data.portfolio} />
-          </div>
-          <UIItems items={portfolioResult.data.portfolio.portfolioItems} />
-          <UIFooter />
-        </main>
-      )}
+      <div> <i>TODO:</i>Update this page </div>
     </div>
   );
 };
